b37cc0b52828faef31fd4cdeec5ce984c34af4c9
Alberto Mori <a.mori@abletech.it>
2025-03-07 16:36:11 +0100

    fix return types on video retriever service

---
+++ src/Sentimeter.Web.Api/Services/YouTubeVideoRetriever.cs
@@ -0,0 +1,28 @@
+﻿using Google.Apis.YouTube.v3;
+using Sentimeter.Web.Models;
+
+namespace Sentimeter.Web.Api.Services;
+
+public class YouTubeVideoRetriever(YouTubeService youtubeService) : IVideoRetriever
+{
+    public async Task<DiscoveryVideoInformationResponseModel?> DiscoveryVideoInformationAsync(DiscoveryVideoInformationModel model)
+    {
+        var videoRequest = youtubeService.Videos.List("snippet");
+        videoRequest.Id = model.VideoId;
+
+        var videos = await videoRequest.ExecuteAsync();
+
+        var videoItem = videos.Items.FirstOrDefault();
+        if (videoItem == null)
+        {
+            return null;
+        }
+
+        var publishedAt = videoItem.Snippet.PublishedAtDateTimeOffset;
+
+        return new DiscoveryVideoInformationResponseModel(
+            videoItem.Snippet.Title,
+            videoItem.Snippet.Description,
+            publishedAt.HasValue ? null : publishedAt!.Value.DateTime);
+    }
+}
