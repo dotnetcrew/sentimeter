1c98f0a6634f998cc7e9bc0127e0843a7584db9c
Alberto Mori <a.mori@abletech.it>
2025-04-04 16:41:25 +0200

    pagination for video list

--- src/Sentimeter.Web.App/Components/Pages/Videos.razor.cs
+++ src/Sentimeter.Web.App/Components/Pages/Videos.razor.cs
@@ -1,21 +1,35 @@
-﻿using Sentimeter.Web.App.Services;
+﻿using Microsoft.AspNetCore.Components;
+using Sentimeter.Web.App.Services;
 using Sentimeter.Web.Models.Videos;
 
 namespace Sentimeter.Web.App.Components.Pages;
 
 public partial class Videos(VideosApiClient videosApiClient)
 {
+    [Parameter]
+    public int Page { get; set; } = 1;
+
     private VideoListModel model = new();
 
     private bool loading = false;
 
     protected override async Task OnInitializedAsync()
     {
+        if (Page <= 0)
+        {
+            Page = 1;
+        }
+
+        await LoadVideosAsync();
+    }
+
+    private async Task LoadVideosAsync()
+    {
         loading = true;
 
         try
         {
-            model = await videosApiClient.GetVideosAsync(1, 12);
+            model = await videosApiClient.GetVideosAsync(Page, 12);
         }
         finally
         {
