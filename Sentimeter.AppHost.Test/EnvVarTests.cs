5be06e9fb335bcf6e75949e856dd2197a720be5d
Daniele Lombardo <d.lombardo@abletech.it>
2025-05-16 16:17:46 +0200

    NuGet packages updates and introduce Test project for AppHost

---
+++ Sentimeter.AppHost.Test/EnvVarTests.cs
@@ -0,0 +1,36 @@
+﻿using Aspire.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://learn.microsoft.com/en-us/dotnet/aspire/testing/write-your-first-test?pivots=xunit
+
+namespace Sentimeter.AppHost.Test;
+
+public class EnvVarTests
+{
+    [Fact]
+    public async Task WebResourceEnvVarsResolveToApiService()
+    {
+        // Arrange
+        var builder = await DistributedApplicationTestingBuilder
+            .CreateAsync<Projects.Sentimeter_AppHost>();
+
+        var frontend = builder.CreateResourceBuilder<ProjectResource>("sentimeter-webapp");
+
+        // Act
+        var envVars = await frontend.Resource.GetEnvironmentVariableValuesAsync(
+            DistributedApplicationOperation.Publish);
+
+        // Assert
+        Assert.Contains(envVars, static (kvp) =>
+        {
+            var (key, value) = kvp;
+
+            return key is "services__sentimeter-webapi__https__0"
+                && value is "{sentimeter-webapi.bindings.https.url}";
+        });
+    }
+}
---
+++ Sentimeter.AppHost.Test/MyIntegrationTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentimeter.AppHost.Test;
+
+
+public class MyIntegrationTest
+{
+    [Fact]
+    public async Task GetWebResourceRootReturnsOkStatusCode()
+    {
+        // Arrange
+        var builder = await DistributedApplicationTestingBuilder
+            .CreateAsync<Projects.Sentimeter_AppHost>();
+
+        builder.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+
+        // To output logs to the xUnit.net ITestOutputHelper, 
+        // consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+
+        await using var app = await builder.BuildAsync();
+
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("sentimeter-webapp");
+
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+        await app.ResourceNotifications.WaitForResourceHealthyAsync(
+            "sentimeter-webapp",
+            cts.Token);
+
+        var response = await httpClient.GetAsync("/");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
